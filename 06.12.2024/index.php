2b6f5171d92e579225a85fbcf8796a40e538ffae
R.Kirushanth <139618570+RKirushanth@users.noreply.github.com>
2024-12-08 08:57:39 +0000

    index added

---
+++ 06.12.2024/index.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>2019ASP85</h1><br><br>
+
+    <a href="getTable.php">Gettable</a><br>
+    
+    <a href="myfunc.php">MyFunction</a><br>
+
+
+    
+</body>
+</html>
